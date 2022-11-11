e7c230cc081ed299a0f3cb1ec627d7713dc9d555
ManelMayoral <manelmayoralcano7@gmail.com>
2022-11-11 10:44:52 +0100

    juan

--- app/src/main/java/com/example/grancentre/restaurantes.java
+++ app/src/main/java/com/example/grancentre/restaurantes.java
@@ -2,13 +2,30 @@ package com.example.grancentre;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class restaurantes extends AppCompatActivity {
+public class restaurantes extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_restaurantes);
+        setContentView(R.layout.activity_main);
+
+        View bhome = findViewById(R.id.bhome);
+        bhome.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        View v = (View)view;
+
+        if (v.getId() == R.id.bhome ) {
+            //Obrim l'activitat 2 amb el Intent
+
+            Intent intent1 = new Intent(this,MainActivity.class);  //.class és l'arxiu java compilat
+            startActivity(intent1);
+
+        }
     }
 }
